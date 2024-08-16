85b7525a794918927e68a4f0365c9bf0a9b47144
Michal Sokolowski <0michalsokolowski0@gmail.com>
2024-08-16 13:31:29 +0200

    A change

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
-resource "null_resource" "example" {
-  provisioner "local-exec" {
-    command = "echo This command will execute whenever the configuration changes"
-  }
+resource "spacelift_context" "test" {
+  name = "My first context"
+}
+
+resource "spacelift_environment_variable" "test" {
+  context_id  = spacelift_context.test.id
+  name        = "BACON"
+  value       = "is tasty"
+  description = "Bacon is tasty"
 }
