2bd069f8d77b12261fa97802b659aaae677211a6
Michal Sokolowski <0michalsokolowski0@gmail.com>
2024-08-29 16:08:07 +0200

    Comment out evenerything

--- main.tf
+++ main.tf
@@ -6,15 +6,15 @@ terraform {
   }
 }
 
-resource "spacelift_space" "dev-test9999999" {
-  name = "terraform-dev-teaaaaaata"
-  parent_space_id = "root"
-}
-
-resource "spacelift_space" "dev-test88888" {
-  name = "terraform-dev-teaaahggaata"
-  parent_space_id = "root"
-}
+# resource "spacelift_space" "dev-test9999999" {
+#   name = "terraform-dev-teaaaaaata"
+#   parent_space_id = "root"
+# }
+#
+# resource "spacelift_space" "dev-test88888" {
+#   name = "terraform-dev-teaaahggaata"
+#   parent_space_id = "root"
+# }
 
 # resource "spacelift_space" "dev-test" {
 #   name = "terraform-dev-third"
