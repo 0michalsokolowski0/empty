38d016993d81c69b77f943c2239782d673487fe9
Michal Sokolowski <0michalsokolowski0@gmail.com>
2025-02-05 10:28:24 +0100

    chore: An update

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 # Create 2000 random passwords with max length with different resource names
 resource "random_password" "password" {
   count            = 2000
-  length           = 16
+  length           = 32
   special          = true
   override_special = "!#$%&*()-_=+[]{}<>:?"
 }
