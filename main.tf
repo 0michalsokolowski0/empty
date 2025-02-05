11bbe4f7b64b5a74ca3d77ffff21ecc26787a2b2
Michal Sokolowski <0michalsokolowski0@gmail.com>
2025-02-05 10:42:27 +0100

    chore: An update

--- main.tf
+++ main.tf
@@ -6,17 +6,17 @@ terraform {
   }
 }
 
-resource "random_password" "password" {
-  count            = 1
-  length           = 32
-  special          = true
-  override_special = "!#$%&*()-_=+[]{}<>:?"
-}
-resource "random_password" "passwords" {
-  count            = 1500
-  length           = 32
-  special          = true
-  override_special = "!#$%&*()-_=+[]{}<>:?"
-}
+# resource "random_password" "password" {
+#   count            = 1
+#   length           = 32
+#   special          = true
+#   override_special = "!#$%&*()-_=+[]{}<>:?"
+# }
+# resource "random_password" "passwords" {
+#   count            = 1500
+#   length           = 32
+#   special          = true
+#   override_special = "!#$%&*()-_=+[]{}<>:?"
+# }
 
 
