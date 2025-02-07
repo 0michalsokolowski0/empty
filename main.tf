b43b9b7f22a10f392e35c2db6f636c07ac07ab7c
Michal Sokolowski <0michalsokolowski0@gmail.com>
2025-02-07 10:59:31 +0100

    chore: An update

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 resource "random_password" "passwords" {
-  count            = 1300
+  count            = 1350
   length           = 32
   special          = true
   override_special = "!#$%&*()-_=+[]{}<>:?"
